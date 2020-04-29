148bbbe46524776624e7a5851e71f0826bab6a93
Minsoo Park <topangel777@gmail.com>
2020-04-29 10:29:49 +0900

    Update git.sh

--- git.sh
+++ git.sh
@@ -2,3 +2,4 @@ git config --global user.name "${1}"
 git config --global user.email "${2}"
 git config --global push.default simple
 git config --global core.editor vim
+git config --global diff.tool vimdiff
